0b1e7ed665d1472bee72a7e2a427284ed0fa9a97
unknown <peedroaleex@gmail.com>
2022-09-22 01:09:41 -0300

    cambio de gochi a mihuertafacilversiodos

--- src/components/view/cultivo/componentes/Archivos.jsx
+++ src/components/view/cultivo/componentes/Archivos.jsx
@@ -2,17 +2,20 @@ import React from "react";
 
 const Archivos = (props) => {
   const listaArchivos = props.infoarchivo;
-  const Archivos = listaArchivos.map((publicidad, index) => (
+  const Archivos = listaArchivos.map((archi, index) => (
     <div key={index} class="my-3 mx-auto text-center">
-      <p className="fs-5 fw-bold">{publicidad.nombre}</p>
-      
-        <a href={publicidad.enlace}><img src="https://img.icons8.com/office/80/000000/export-pdf.png"alt="imagen"/>
-        </a><p>{publicidad.descripcion}</p>
+      <p className="fs-5 fw-bold">{archi.nombre}</p>
+
+      <a href={archi.enlace} target="_blank" rel="noopener noreferrer">
+        <img
+          src="https://img.icons8.com/office/80/000000/export-pdf.png"
+          alt="imagen"
+        />
+      </a>
+      <p>{archi.descripcion}</p>
     </div>
   ));
   return <div class="d-flex flex-column my-3 bg-white">{Archivos}</div>;
 };
-
- 
 
 export default Archivos;
